64a4d8ee65620ebab3bd75a6f1231267e6183fd2
lukaszKielar <kielar.lukasz@hotmail.com>
2020-09-02 20:43:13 +0200

    main.rs clean-up

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,24 @@ use std::convert::From;
 use std::io;
 
 use dotenv::dotenv;
-use env_logger;
-use log::debug;
 
 use crate::metadata::Metadata;
-use crate::package::print_package;
-use crate::package::Package;
-use crate::utils::get_site_packages_path;
+use crate::package::{print_package, Package};
+use crate::utils::get_conda_metadata;
+use crate::utils::get_dependent_packages;
 
 fn main() -> io::Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    // get site-packages folder for the conda env that is activated
-    let site_packages = get_site_packages_path();
-
-    // iterate over entries in site-packages
-    // and look for folder that endswith `info`
-    // (metadata files are stored there)
-    for entry in site_packages.read_dir()? {
-        let dir = entry?.path();
-        if dir.is_dir() & dir.to_str().unwrap().ends_with("egg-info") {
-            debug!("{:?}", dir);
-        }
-    }
+    // let conda_metadata = get_conda_metadata();
+    // for entry in conda_metadata.keys() {
+    //     println!("{:?}", conda_metadata.get(entry).unwrap());
+    // }
+    println!("{:?}", get_dependent_packages("pip".to_string()));
 
     println!("----package----");
-    let name = String::from("dask-core");
+    let name = String::from("pip");
     let metadata = Metadata::from_name(name).unwrap();
     let package = Package::from(metadata);
     print_package(&package);
