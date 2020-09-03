11b21833ed2adb1fcc7176b9e7070270207ec200
lukaszKielar <luk.kielar@gmail.com>
2020-09-03 21:47:06 +0200

    main clean-up

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,33 @@ mod metadata;
 mod package;
 mod utils;
 
-use std::convert::From;
 use std::io;
 
 use dotenv::dotenv;
 
 use crate::metadata::Metadata;
 use crate::package::{print_package, Package};
-use crate::utils::get_conda_metadata;
 use crate::utils::get_dependent_packages;
+use crate::utils::get_leaves;
 
 fn main() -> io::Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    // let conda_metadata = get_conda_metadata();
-    // for entry in conda_metadata.keys() {
-    //     println!("{:?}", conda_metadata.get(entry).unwrap());
-    // }
     println!("{:?}", get_dependent_packages("pkg1"));
+    println!();
 
     println!("----package----");
     let name = "pkg3";
     let p: Package = Metadata::from_name(name).unwrap().into();
     print_package(&p);
+    println!();
+
+    println!("----leaves----");
+    for leaf in get_leaves().iter() {
+        println!("{}", leaf)
+    }
+    println!();
 
     Ok(())
 }
