144cef352d727d34ab91d61ce17e9a6e9f906077
lukaszKielar <kielar.lukasz@hotmail.com>
2020-09-07 21:12:29 +0200

    impl couple of From for CondaEnv

--- src/env.rs
+++ src/env.rs
@@ -1,27 +1,19 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
 
+use crate::metadata::Metadata;
+use crate::package::{Installer, Package};
+
+// TODO add unit tests
 pub struct CondaEnv {
-    // TODO name should be automatically taken from CONDA_DEFAULT_ENV variable
     name: String,
-    // TODO dependencies should be defined as a Vec<Package>
-    dependencies: Vec<String>,
-    // TODO pip should be defined as a Vec<Package>
-    // TODO pip should be deleted, information about installer is available in Package struct
-    pip: Option<Vec<String>>,
+    installed_packages: Vec<Package>,
 }
 
 impl CondaEnv {
-    pub fn new(name: String, dependencies: Vec<String>, pip: Option<Vec<String>>) -> Self {
-        Self {
-            name,
-            dependencies,
-            pip,
-        }
-    }
-
     // TODO add information about version
     pub fn to_yml(&self) -> io::Result<()> {
         // create a file in current directory
@@ -36,23 +28,77 @@ impl CondaEnv {
 
         // write dependencies
         file.write_all(b"dependencies:")?;
-        for d in self.dependencies.iter() {
+
+        // get deps installed by conda
+        let conda_deps: Vec<&Package> = self
+            .installed_packages
+            .iter()
+            .filter(|&p| p.installer == Installer::Conda)
+            .collect();
+
+        // get deps installed by pip
+        let pip_deps: Vec<&Package> = self
+            .installed_packages
+            .iter()
+            .filter(|&p| p.installer == Installer::Pip)
+            .collect();
+
+        for &d in conda_deps.iter() {
+            let p_string: String = d.to_owned().into();
             file.write_all(b"\n")?;
             file.write_all(b"  - ")?;
-            file.write_all(d.as_bytes())?;
+            file.write_all(p_string.as_bytes())?;
         }
         file.write_all(b"\n")?;
 
-        if let Some(pip_dependencies) = &self.pip {
+        if pip_deps.len() > 0 {
             file.write_all(b"  - pip:")?;
-            for d in pip_dependencies.iter() {
+            for &d in pip_deps.iter() {
+                let p_string: String = d.to_owned().into();
                 file.write_all(b"\n")?;
                 file.write_all(b"    - ")?;
-                file.write_all(d.as_bytes())?;
+                file.write_all(p_string.as_bytes())?;
             }
             file.write_all(b"\n")?;
         }
 
         Ok(())
+    }
+}
+
+impl From<HashMap<String, Metadata>> for CondaEnv {
+    fn from(conda_metadata: HashMap<String, Metadata>) -> Self {
+        // get conda name
+        let name =
+            env::var("CONDA_DEFAULT_ENV").expect("Please make sure you have conda env activated.");
+        let installed_packages: Vec<Package> = conda_metadata
+            .values()
+            .map(|m| {
+                let p: Package = m.to_owned().into();
+                p
+            })
+            .collect();
+
+        Self {
+            name,
+            installed_packages,
+        }
+    }
+}
+
+impl From<Vec<String>> for CondaEnv {
+    fn from(leaves: Vec<String>) -> Self {
+        // get conda name
+        let name =
+            env::var("CONDA_DEFAULT_ENV").expect("Please make sure you have conda env activated.");
+        let installed_packages: Vec<Package> = leaves
+            .iter()
+            .map(|name| Metadata::from_name(name).unwrap().into())
+            .collect();
+
+        Self {
+            name,
+            installed_packages,
+        }
     }
 }
