46bd373c8cf6f7db1945bc235cbe0eb3576f0ce9
lukaszKielar <kielar.lukasz@hotmail.com>
2020-10-06 20:37:36 +0200

    first attempt to cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-mod cli;
 mod env;
 mod metadata;
 mod package;
@@ -7,6 +6,7 @@ mod utils;
 use std::io;
 
 use dotenv::dotenv;
+use structopt::StructOpt;
 
 use crate::env::CondaEnv;
 use crate::metadata::Metadata;
@@ -14,38 +14,64 @@ use crate::package::{print_package, Package};
 use crate::utils::get_dependent_packages;
 use crate::utils::get_leaves;
 
-fn cli_main() {
-    dotenv().ok();
+// FIXME better to use enum
+#[derive(Debug, StructOpt)]
+struct Cli {
+    #[structopt(short = "p", long, conflicts_with = "leaves")]
+    package: Option<String>,
 
-    let matches = cli::build_cli().get_matches();
+    /// Prints top level packages in conda environment
+    #[structopt(short = "l", long, conflicts_with = "package")]
+    leaves: bool,
 
-    match matches.subcommand() {
-        _ => unreachable!("The cli parser should prevent reaching here"),
-    }
+    /// Prints packages installed by conda
+    #[structopt(long)]
+    no_pip: Option<bool>,
 }
 
 fn main() -> io::Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    println!("{:?}", get_dependent_packages("pkg1"));
-    println!();
+    let cli: Cli = Cli::from_args();
+    print!("{:?}", cli);
 
-    println!("----package----");
-    let name = "pkg3";
-    let p: Package = Metadata::from_name(name)?.into();
-    print_package(&p);
-    println!();
-
-    println!("----leaves----");
-    let leaves = get_leaves();
-    for leaf in leaves.iter() {
-        println!("{}", leaf)
+    match cli {
+        Cli { package, .. } => match package {
+            Some(name) => {
+                let p: Package = Metadata::from_name(name)?.into();
+                println!();
+                print_package(&p);
+            }
+            None => (),
+        },
+        // TODO check no_pip value!!
+        Cli { leaves, .. } => {
+            let leaves = get_leaves();
+            for leaf in leaves.iter() {
+                println!("{}", leaf)
+            }
+        }
     }
 
-    println!("----leaves-to-environment.yml----");
-    let env: CondaEnv = leaves.into();
-    env.to_yml()?;
+    // println!("{:?}", get_dependent_packages("pkg1"));
+    // println!();
+
+    // println!("----package----");
+    // let name = "pkg3";
+    // let p: Package = Metadata::from_name(name)?.into();
+    // print_package(&p);
+    // println!();
+
+    // println!("----leaves----");
+    // let leaves = get_leaves();
+    // for leaf in leaves.iter() {
+    //     println!("{}", leaf)
+    // }
+
+    // println!("----leaves-to-environment.yml----");
+    // let env: CondaEnv = leaves.into();
+    // env.to_yml()?;
 
     Ok(())
 }
